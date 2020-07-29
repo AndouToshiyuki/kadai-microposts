f809507d36a8f8781e27df62e547b363bb377456
EC2 Default User <ec2-user@ip-172-31-60-94.ec2.internal>
2020-07-29 01:33:40 +0000

    favorite

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -2,17 +2,17 @@ class FavoritesController < ApplicationController
   before_action :require_user_logged_in
   
   def create
-    micropost = Favorites.find(params[:micropost_id],[:user_id])
+    micropost = Micropost.find_by(params[:micropost_id])
     current_user.favorite(micropost)
     flash[:success] = '投稿をいいねしました。'
-    redirect_to micropost
+    redirect_to root_url
   end
 
   def destroy
-    micropost = Favorites.find(params[:micropost_id],[:user_id])
+    micropost = Micropost.find_by(params[:micropost_id])
     current_user.unfavorite(micropost)
     flash[:success] = 'いいねを外しました。'
-    redirect_to micropost
+    redirect_to root_url
   end
   
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -35,15 +35,15 @@ class User < ApplicationRecord
   end
   
   def favorite(micropost)
-    favorites.find_or_create_by(micropost.id)
+    favorites.find_or_create_by(micropost_id: micropost.id)
   end
   
   def unfavorite(micropost)
-    favorite = favorites.find_or_create_by(micropost.id)
+    favorite = favorites.find_or_create_by(micropost_id: micropost.id)
     favorite.destroy if favorite
   end
   
-  def favorites?(micropost)
-    favorites.where(micropost_id: micropost.id).exists?
+  def favorited_micropost?(micropost)
+    self.favorited_microposts.include?(micropost)
   end
 end
